6f8cbe7408df7e505660371e1245472d5a630adf
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2023-03-31 15:50:13 +0530

    Changes

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../components/Home";
+import Navbar from "../components/Navbar";
 
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navbar />} />
       </Routes>
     </div>
   );
